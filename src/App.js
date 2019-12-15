71b74d3874be2184658011e5b24c5c6fe91c543e
CAM603 <cameronhawley@comcast.net>
2019-12-15 14:24:51 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
