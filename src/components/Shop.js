623074ef796855a0a076641fd84aeea2727db598
CAM603 <cameronhawley@comcast.net>
2019-12-15 14:44:07 -0500

    fetching pokemon

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 
 function Shop() {
+    useEffect(() => {
+        fetchItems();
+    }, [])
+    const fetchItems = async () => {
+        const data = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const items = await data.json();
+        console.log(items)
+    }
     return (
     <div>
         <h1>Shop</h1>
